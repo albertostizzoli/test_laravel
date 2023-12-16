4befe880a7055d42175418c3d9db511b826e55c7
albertostizzoli <albertostizzoli@yahoo.it>
2023-12-16 17:13:15 +0100

    first commit

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,12 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
-});
+    return view('home');
+})->name('home');
+
+Route::get('/about', function () {
+    $name = 'John';
+    $surname = 'Doe';
+    return view('pages.about', compact('name','surname'));
+})->name('about');
+
